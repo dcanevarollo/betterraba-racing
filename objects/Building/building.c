a6b8b233e8e7c6bc41076a75a098c05f2854589b
Douglas Canevarollo <douglascanevarollo@gmail.com>
2019-10-31 09:26:54 -0300

    Modulazired building file

--- objects/Building/building.c
+++ objects/Building/building.c
@@ -1,14 +1,4 @@
 #include "building.h"
-#include "../../utils/Color/color.h"
-#include <math.h>
-
-#define RADIAN_INCR 0.017453293
-
-int cameraRadius = 40;
-int camPosX = 0, camPosY = 10, camPosZ = 40;
-int camLookX = 0, camLookY = 0, camLookZ = 0;
-int camAxisX = 0, camAxisY = 1, camAxisZ = 0;
-float camAngle = 0;
 
 /**
  * @author Douglas Canevarollo
@@ -26,77 +16,4 @@ void buildBuilding(Color color, float height, float size) {
   glutSolidCube(size);
 
   glPopMatrix();
-}
-
-void display() {
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_LINE_SMOOTH);
-  glEnable(GL_POLYGON_SMOOTH);
-  glEnable(GL_SMOOTH);
-  glEnable(GL_BLEND);
-
-  Color backgroundColor = white();
-  glClearColor(backgroundColor.red, backgroundColor.green, backgroundColor.blue, 0);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-
-  gluPerspective(45, 1, 1, 50);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-
-  gluLookAt(camPosX, camPosY, camPosZ, camLookX, camLookY, camLookZ, camAxisX, camAxisY, camAxisZ);
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  Color buildingColor = purple();
-  buildBuilding(buildingColor, 15, 10);
-
-  glutSwapBuffers();
-}
-
-void rotateCamera(int multiplier) {
-  camAngle += (multiplier * RADIAN_INCR);
-
-  camPosX = cameraRadius * sin(camAngle);
-  camPosZ = cameraRadius * cos(camAngle);
-}
-
-void keyboard(unsigned char key, int x, int y) {
-  switch (key) {
-    case 'a':
-      rotateCamera(-1);  // Rotates the camera to the left.
-      break;
-
-    case 'd':
-      rotateCamera(1);  // Rotates the camera to the right.
-      break;
-
-    case 'q':
-      glutExit();
-      exit(0);
-      break;
-
-    default:
-      break;
-  }
-
-  glutPostRedisplay();
-}
-
-int main(int argc,char **argv) {
-  glutInit(&argc, argv);
-
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-
-  glutInitWindowSize(800, 800);
-  glutInitWindowPosition(0, 0);
-  glutCreateWindow("The Bouncing Ball");
-
-  glutDisplayFunc(display);
-  glutKeyboardFunc(keyboard);
-
-  glutMainLoop();
-
-  return 0;
 }
