c0f0a4be13dac84ad47dc83717c6b30ec729e3c4
Rafael Campos <rafael.f.campos@unesp.br>
2019-11-11 09:43:50 -0300

    Criação do cenário da floresta.

---
+++ scenarios/Florest/florest.c
@@ -0,0 +1,86 @@
+#include "florest.h"
+
+/**
+ * @author Rafael Campos
+ * Arquivo que define o método para construir a pista floresta. 
+ */
+
+/*  Método para construir o cenário da floresta, chama as funções para construir a rua, 
+    as partes da floresta na direita e esquerda e as funções para criar as árvores e arbustos. 
+*/
+void buildFlorestScenario() {
+    buildStreet();
+    buildFlorestFloor();
+
+    glPushMatrix();
+        glTranslatef(25, 0, 5);
+        buildPineTree();
+        glTranslatef(0, 2, -25);
+        buildRoundBush();
+        glTranslatef(0, 0, -25);
+        buildRoundTree();
+        glTranslatef(0, 0, -25);
+        buildPineBush();
+        glTranslatef(0, 0, -25);
+        buildPineTree();
+        glTranslatef(-2, 0, -25);
+        buildRoundTree();
+        glTranslatef(0, 2, -25);
+        buildPineBush();
+        glTranslatef(0, 2, -25);
+        buildRoundBush();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(-25, 2, 5);
+        buildPineBush();
+        glTranslatef(0, 0, -25);
+        buildRoundTree();
+        glTranslatef(0, 2, -25);
+        buildRoundBush();
+        glTranslatef(0, 0, -25);
+        buildPineTree();
+        glTranslatef(0, 2, -25);
+        buildPineBush();
+        glTranslatef(3, 2, -25);
+        buildRoundBush();
+        glTranslatef(2, 0, -25);
+        buildPineTree();
+        glTranslatef(2, 0, -25);
+        buildRoundTree();
+
+    glPopMatrix();
+}
+
+/* Método para criar o chão da floresta que ficará ao redor da rua. */
+void buildFlorestFloor() {
+    glPushMatrix();
+
+        Color florestFloorColor = darkGreen();
+        glScalef(2, 3, 2);
+        glColor3ub(florestFloorColor.red, florestFloorColor.green, florestFloorColor.blue);
+
+         glBegin(GL_QUADS);
+            glVertex3f(10, 0, -100);
+            glVertex3f(10, 0, 100);
+            glVertex3f(40, 0, 100);
+            glVertex3f(40, 0, -100);
+            glVertex3f(10, 0, -500);
+            glVertex3f(10, 0, 150);
+            glVertex3f(100, 0, 150);
+            glVertex3f(100, 0, -500);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glVertex3f(-10, 0, -100);
+            glVertex3f(-10, 0, 100);
+            glVertex3f(-40, 0, 100);
+            glVertex3f(-40, 0, -100);
+            glVertex3f(-10, 0, -500);
+            glVertex3f(-10, 0, 150);
+            glVertex3f(-100, 0, 150);
+            glVertex3f(-100, 0, -500);
+        glEnd();
+
+    glPopMatrix();
+}
